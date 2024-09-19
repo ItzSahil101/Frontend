c22e19692fb6606cb86faa471ebcc2018a9de3f5
SAHIL <154607992+ItzSahil101@users.noreply.github.com>
2024-09-19 20:44:05 +0545

    Speed & Anylitcs vercel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
