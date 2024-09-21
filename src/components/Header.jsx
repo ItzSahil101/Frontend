ae79e0b725dba512092bd5cc20127bfa53e8e1a2
SAHIL <154607992+ItzSahil101@users.noreply.github.com>
2024-09-21 08:06:18 +0545

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -74,6 +74,7 @@ function Header(isLoggedIn) {
               })}
             </ul>
           </li>
+          <li><Link to="/privacy">Privacy</Link></li>
           <li><Link className="no-underline font-semibold" to="#" onClick={() => { toggleTheme() }}>
       
           <input type="checkbox" className="checkbox" id="checkbox"/>
